f310f2f39c415788e97d7018d54069d20f9328e4
ret1rn <92985035+oooooooohmygod@users.noreply.github.com>
2023-12-10 16:49:57 +0800

    Update main.cpp

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
-using namespace std;
+//using namespace std;
 int main() {
+    system("powershell winget update");
     system("powershell winget install Microsoft.OpenJDK.17");
     return 0;
 }
